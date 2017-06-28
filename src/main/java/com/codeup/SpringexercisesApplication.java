09ad921b1fa84b55a4cf8809f6508bdba6a0af65
JSR7 <jsran01@yahoo.com>
2017-06-28 11:10:56 -0500

    made edits for maven connection

--- src/main/java/com/codeup/SpringexercisesApplication.java
+++ src/main/java/com/codeup/SpringexercisesApplication.java
@@ -1,12 +1,15 @@
-package com.codeup;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringexercisesApplication {
-
+public class BlogApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
-		SpringApplication.run(SpringexercisesApplication.class, args);
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BlogApplication.class);
 	}
 }
